37a98b9b867a3f383336eb1fbe37ac2b9735bf56
brianisadog <s254901282002@gmail.com>
2019-04-25 19:44:08 -0700

    Added interactivity and write-ups

--- vis.js
+++ vis.js
@@ -1,6 +1,5 @@
 const attr = {
-    file: "./data/fire_department_calls_for_service_sample_stratified.json",
-    // file: "./data/fire_department_calls_for_service_stratified.json",
+    file: "./data/fire_department_calls_for_service_stratified.json",
     plot: {
         width: 960,
         height: 500
@@ -28,14 +27,16 @@ const translate = function(a, b) {
 const drawLinks = function(g, links, generator) {
     let paths = g
         .selectAll("path")
-        .data(links)
+        .data(links, link => link.target.data.name)
         .enter()
         .append("path")
         .attr("d", generator)
         .attr("class", "link");
+
+    return paths;
 };
 
-const drawNodes = function(g, nodes, raise, color) {
+const drawNodes = function(g, nodes, color) {
     let circles = g
         .selectAll("circle")
         .data(nodes, node => node.data.name)
@@ -44,42 +45,70 @@ const drawNodes = function(g, nodes, raise, color) {
         .attr("r", d => (d.r ? d.r : attr.r))
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
-        .attr("id", d => d.data.name)
         .attr("class", "node")
         .style("fill", d => color(d.depth));
 
-    setupEvents(g, circles, raise);
+    return circles;
 };
 
-const setupEvents = function(g, selection, raise) {
-    selection.on("mouseover.highlight", function(d) {
+const setupEvents = function(plot, nodes, paths, raise) {
+    nodes.on("mouseover.highlight", function(d) {
         let path = d3
             .select(this)
             .datum()
-            .path(selection.data()[0]);
-        let update = selection.data(path, node => node.data.name);
-        update.classed("active", true);
+            .path(nodes.data()[0]);
+        let updateNodes = nodes.data(path, node => node.data.name);
+        updateNodes.classed("active", true);
 
         if (raise) {
-            update.raise();
+            updateNodes.raise();
+        }
+
+        // highlighting paths with path's target name
+        if (paths) {
+            let updatePaths = plot.selectAll("path.link").filter(function(d) {
+                let check = false;
+                path.forEach(function(p) {
+                    if (p.data.name === d.target.data.name) {
+                        check = true;
+                    }
+                });
+                return check;
+            });
+
+            updatePaths.classed("active", true);
         }
     });
 
-    selection.on("mouseout.highlight", function(d) {
+    nodes.on("mouseout.highlight", function(d) {
         let path = d3
             .select(this)
             .datum()
-            .path(selection.data()[0]);
-        let update = selection.data(path, node => node.data.name);
-        update.classed("active", false);
+            .path(nodes.data()[0]);
+        let updateNodes = nodes.data(path, node => node.data.name);
+        updateNodes.classed("active", false);
+
+        if (paths) {
+            let updatePaths = plot.selectAll("path.link").filter(function(d) {
+                let check = false;
+                path.forEach(function(p) {
+                    if (p.data.name === d.target.data.name) {
+                        check = true;
+                    }
+                });
+                return check;
+            });
+
+            updatePaths.classed("active", false);
+        }
     });
 
-    selection.on("mouseover.tooltip", function(d) {
-        showTooltip(g, d3.select(this));
+    nodes.on("mouseover.tooltip", function(d) {
+        showTooltip(plot, d3.select(this));
     });
 
-    selection.on("mouseout.tooltip", function(d) {
-        g.select("#tooltip").remove();
+    nodes.on("mouseout.tooltip", function(d) {
+        plot.select("#tooltip").remove();
     });
 };
 
@@ -142,7 +171,7 @@ const processData = function(data) {
 };
 
 const sorting = function(a, b) {
-    return b.height - a.height || b.value - a.value;
+    return a.height - b.height || a.value - b.value;
 };
 
 const toCartesian = function(r, theta) {
@@ -152,12 +181,57 @@ const toCartesian = function(r, theta) {
     };
 };
 
+const titleAndLegend = function(svg, title, data, color) {
+    let range = [...Array(data.height + 1).keys()];
+
+    svg.append("text")
+        .attr("transform", translate(attr.pad, 2.5 * attr.pad))
+        .attr("class", "title")
+        .text(title);
+
+    let legend = svg
+        .append("g")
+        .attr(
+            "transform",
+            translate(attr.plot.width - (data.height + 1) * 50 - 10, attr.pad)
+        );
+
+    legend
+        .append("text")
+        .attr("x", -50)
+        .attr("y", 15)
+        .attr("class", "subtitle")
+        .text("Depth");
+
+    legend
+        .selectAll("rect")
+        .data(range)
+        .enter()
+        .append("rect")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("class", "legendRect")
+        .attr("x", d => d * 50)
+        .style("fill", d => color(d))
+        .style("stroke", "#ffffff");
+
+    legend
+        .selectAll(".attribute")
+        .data(range)
+        .enter()
+        .append("text")
+        .attr("x", d => d * 50 + 20)
+        .attr("y", 12)
+        .attr("class", "attribute")
+        .text(d => d);
+};
+
 const drawNodeLink = function(data, color) {
     let layout = d3
         .tree()
         .size([
             attr.plot.width - 2 * attr.pad,
-            attr.plot.height - 2 * attr.pad
+            attr.plot.height - 4 * attr.pad
         ]);
 
     layout(data);
@@ -166,12 +240,16 @@ const drawNodeLink = function(data, color) {
     let plot = svg
         .append("g")
         .attr("id", "plot1")
-        .attr("transform", translate(attr.pad, attr.pad));
+        .attr("transform", translate(attr.pad, 3 * attr.pad));
 
     let raise = true;
+    let paths = drawLinks(plot.append("g"), data.links(), linkTypes.curvedLine);
+    let circles = drawNodes(plot.append("g"), data.descendants(), color);
 
-    drawLinks(plot.append("g"), data.links(), linkTypes.curvedLine);
-    drawNodes(plot.append("g"), data.descendants(), raise, color);
+    setupEvents(plot, circles, paths, raise);
+
+    let title = "Dendrogram";
+    titleAndLegend(svg, title, data, color);
 };
 
 const drawDenTree = function(data, color) {
@@ -199,9 +277,13 @@ const drawDenTree = function(data, color) {
         );
 
     let raise = true;
+    let paths = drawLinks(plot.append("g"), data.links(), linkTypes.radialLine);
+    let circles = drawNodes(plot.append("g"), data.descendants(), color);
 
-    drawLinks(plot.append("g"), data.links(), linkTypes.radialLine);
-    drawNodes(plot.append("g"), data.descendants(), raise, color);
+    setupEvents(plot, circles, paths, raise);
+
+    let title = "Circular Tree";
+    titleAndLegend(svg, title, data, color);
 };
 
 const drawTreeMap = function(data, color) {
@@ -210,7 +292,7 @@ const drawTreeMap = function(data, color) {
         .padding(attr.r)
         .size([
             attr.plot.width - 2 * attr.pad,
-            attr.plot.height - 2 * attr.pad
+            attr.plot.height - 5 * attr.pad
         ]);
 
     layout(data);
@@ -220,9 +302,7 @@ const drawTreeMap = function(data, color) {
     let plot = svg
         .append("g")
         .attr("id", "plot3")
-        .attr("transform", translate(attr.pad, attr.pad));
-
-    console.log(data.descendants());
+        .attr("transform", translate(attr.pad, 4 * attr.pad));
 
     let rects = plot
         .selectAll("rect")
@@ -233,13 +313,113 @@ const drawTreeMap = function(data, color) {
         .attr("y", d => d.y0)
         .attr("width", d => d.x1 - d.x0)
         .attr("height", d => d.y1 - d.y0)
-        .attr("id", d => d.data.name)
         .attr("class", "node")
         .style("fill", d => color(d.depth));
 
     let raise = false;
 
-    setupEvents(plot, rects, raise);
+    setupEvents(plot, rects, null, raise);
+
+    let title = "Treemap";
+    titleAndLegend(svg, title, data, color);
+};
+
+const drawSunburst = function(data, color) {
+    let r = Math.min(attr.plot.width, attr.plot.height) / 2;
+
+    let layout = d3.partition().size([2 * Math.PI, r - attr.pad]);
+
+    layout(data);
+
+    let svg = d3.select("#svg4");
+
+    let plot = svg
+        .append("g")
+        .attr("id", "plot4")
+        .attr(
+            "transform",
+            translate(attr.plot.width / 2, attr.plot.height / 2)
+        );
+
+    let arc = d3
+        .arc()
+        .startAngle(d => d.x0)
+        .endAngle(d => d.x1)
+        .innerRadius(d => d.y0)
+        .outerRadius(d => d.y1);
+
+    let arcs = plot
+        .selectAll("path")
+        .data(data.descendants())
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("class", "node")
+        .style("fill", d => color(d.depth));
+
+    let raise = true;
+    setupEvents(plot, arcs, null, raise);
+
+    let title = "Sunburst";
+    titleAndLegend(svg, title, data, color);
+};
+
+const getModules = function(data) {
+    let modules = data.children.map(function(d) {
+        let name = d.data.name.substring(d.data.name.indexOf("->") + 2);
+        return name.length == 0 ? "Empty" : name;
+    });
+
+    modules.unshift(data.data.name);
+
+    return modules;
+};
+
+const createDropDown = function(modules, data, color) {
+    let drop = d3.select("#drop");
+
+    drop.append("select")
+        .selectAll("option")
+        .data(modules)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+
+    drop.on("change", function() {
+        let value = d3
+            .select(this)
+            .select("select")
+            .property("value");
+
+        removeAll();
+
+        if (value === data.data.name) {
+            visualize(data.copy(), color);
+        } else {
+            for (let i = 0; i < data.children.length; i++) {
+                let name = data.children[i].data.name.substring(
+                    data.children[i].data.name.indexOf("->") + 2
+                );
+                if (name === value) {
+                    visualize(data.children[i].copy(), color);
+                }
+            }
+        }
+    });
+};
+
+const visualize = function(data, color) {
+    drawNodeLink(data, color);
+    drawDenTree(data, color);
+    drawTreeMap(data, color);
+    drawSunburst(data, color);
+};
+
+const removeAll = function() {
+    d3.selectAll("svg")
+        .selectAll("*")
+        .remove();
 };
 
 d3.json(attr.file).then(function(d) {
@@ -247,7 +427,8 @@ d3.json(attr.file).then(function(d) {
     let color = d3.scaleSequential([processed.height, 0], d3.interpolateWarm);
     let sorted = processed.sort(sorting);
 
-    drawNodeLink(sorted, color);
-    drawDenTree(sorted, color);
-    drawTreeMap(sorted, color);
+    let modules = getModules(sorted);
+    createDropDown(modules, sorted, color);
+
+    visualize(sorted, color);
 });
